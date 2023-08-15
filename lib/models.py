c21cabe54138fcd06006b32eea2576181902a707
Aruna Turay <tura_arun@yahoo.com>
2023-08-15 14:06:02 -0400

    pushes migration

--- lib/models.py
+++ lib/models.py
@@ -10,6 +10,7 @@ metadata = MetaData(naming_convention=convention)
 
 Base = declarative_base(metadata=metadata)
 
+# Adds the game model
 class Game(Base):
     __tablename__ = 'games'
 
@@ -19,13 +20,13 @@ class Game(Base):
     platform = Column(String())
     price = Column(Integer())
 
-    reviews = relationship('Review', backref=backref('game'))
+    reviews = relationship('Review', backref=backref('game')) # relationship with the Review table
 
     def __repr__(self):
         return f'Game(id={self.id}, ' + \
             f'title={self.title}, ' + \
             f'platform={self.platform})'
-
+# Adds Review model
 class Review(Base):
     __tablename__ = 'reviews'
 
@@ -33,9 +34,24 @@ class Review(Base):
     score = Column(Integer())
     comment = Column(String())
     
-    game_id = Column(Integer(), ForeignKey('games.id'))
+    game_id = Column(Integer(), ForeignKey('games.id'))  # Adds foreign key that establishes relationship to game
+    user_id = Column(Integer(), ForeignKey('users.id')) # Adds foreign key that establishes relationship to user
 
     def __repr__(self):
         return f'Review(id={self.id}, ' + \
             f'score={self.score}, ' + \
             f'game_id={self.game_id})'
+    
+# adds user model
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    created_at = Column(DateTime(), server_default=func.now())
+    updated_at = Column(DateTime(), onupdate=func.now())
+    reviews = relationship('Review', backref=backref('user')) # Relationship with the Review table
+    # don't forget your __repr__()!
+    def __repr__(self):
+        return f'User(id={self.id}, ' + \
+            f'name={self.name})'
